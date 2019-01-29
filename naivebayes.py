e718210b5fe2c50972f1703b83873e453cf3f074
Andre Hartmann <andrehartmann1993@hotmail.de>
2019-01-29 11:46:55 +0100

    - calculated accuracy, precision, recall and print them

--- naivebayes.py
+++ naivebayes.py
@@ -8,18 +8,19 @@ df = pd.read_csv(
 dataset = df.drop(columns='class')
 dataset_in_vectors_training = dataset.values.tolist()
 
-df_2 = pd.read_csv(
+df_test = pd.read_csv(
     r'C:\Users\Andre\Google Drive\Master Studium Bamberg\AngewandteInformatik\2nd - WS18\KogSys-ML-M\Assignment2\nb_mushrooms_test.csv')
-test_dataset = df_2.drop(columns='class')
+test_dataset = df_test.drop(columns='class')
 dataset_in_vectors_test = test_dataset.values.tolist()
 
 
-def naiveBayesTrain(df):
+def naiveBayesTrain():
     global algorithmNegatives
     algorithmNegatives = 0
     global algorithmPositives
     algorithmPositives = 0
 
+    # Declaration of possible values for each column
     cap_shapes = {'b', 'c', 'x', 'f', 'k', 's'}
     dict_cap_shape_eatable = dict()
     dict_cap_shape_poisonous = dict()
@@ -83,13 +84,13 @@ def naiveBayesTrain(df):
         dict_grill_size_posiouns[gill_size] = nr_poisonous_gill_size / nr_total_gill_size
 
     # Number of eatable mushrooms
-    nr_eatable = df['class'][df['class'] == 'e'].count()
+    nr_eatable = df_test['class'][df_test['class'] == 'e'].count()
 
     # Number of poisonous mushrooms
-    nr_poisonous = df['class'][df['class'] == 'p'].count()
+    nr_poisonous = df_test['class'][df_test['class'] == 'p'].count()
 
     # Total
-    total = df['class'].count()
+    total = df_test['class'].count()
 
     # Probability that a mushroom is eatable
     prob_eatable = nr_eatable / total
@@ -116,7 +117,7 @@ def naiveBayesTrain(df):
         normalized_nb_prob_poisonous = nb_likelihood_poisonous / nb_likelihood_eatable + nb_likelihood_poisonous
         # print(normalized_nb_prob_poisonous)
 
-        if (normalized_nb_prob_eatable > normalized_nb_prob_poisonous):
+        if normalized_nb_prob_eatable > normalized_nb_prob_poisonous:
             algorithmNegatives += 1
         else:
             # The relevant class (the 'positive' class) shall be poisonous(p)
@@ -128,15 +129,20 @@ def naiveBayesTrain(df):
     falsePositives = algorithmPositives-nr_poisonous if algorithmPositives-nr_poisonous >= 0 else 0
     falseNegatives = algorithmNegatives-nr_eatable if algorithmNegatives-nr_eatable >= 0 else 0
 
+    # Accuracy
+    accuracy = (truePositives + trueNegatives) / (truePositives + trueNegatives + falsePositives + falseNegatives)
+    print("Accuracy: %3.8f" % accuracy)
+    # Precision
+    precision = truePositives / (truePositives + falsePositives)
+    print("Precision: %.8f" % precision)
+    # Recall
+    recall = truePositives / (truePositives + falseNegatives)
+    print("Recall: %.8f" % recall)
 
-
-
-    print(algorithmNegatives)
-    print(algorithmPositives)
 
 
 def main():
-    print(naiveBayesTrain(df))
+    print(naiveBayesTrain())
 
 
 if __name__ == "__main__":
